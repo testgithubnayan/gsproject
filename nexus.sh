ea4929df8e4eaeab2eaa874b44488f75f6991153
testgithubnayan <Nayan.soniPro@gmail.com>
2022-06-15 15:23:20 +0530

    case statement in shell

---
+++ nexus.sh
@@ -0,0 +1,37 @@
+read -p   " enter a  num "  num
+case    $num   in
+1)
+echo "sunday"
+;;
+2)
+echo "monday"
+;;
+3)
+echo   "tuesday"
+;;
+4)
+echo  "wednesday"
+;;
+5)
+echo    "thusday"
+;;
+6)
+echo   "friday"
+;;
+7)
+echo  "saturday"
+;;
+*) echo "JAI BHOLENAH JI"
+;;
+
+esac
+
+
+
+
+read -p    "whats your name" name
+
+echo     $name    "hey my name is nayans world"
+
+
+
