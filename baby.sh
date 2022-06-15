89af575ee019d92cd82d96ac1b5fb0abf90ebcbc
testgithubnayan <Nayan.soniPro@gmail.com>
2022-06-15 15:29:40 +0530

    using if else conditon in shell

---
+++ baby.sh
@@ -0,0 +1,34 @@
+	 read  -p "enter a no  =  "   a 
+
+if [ $a  -gt 50 ] ;
+
+then 
+
+printf "  a  is greater than 50"  ;
+
+
+elif  [   $a -le 50 ]  ;
+
+then
+
+printf   "  a is lesser then 50" ;
+
+   
+
+elif [  $a -eq  50  ] ;
+
+then 
+
+
+printf   " a is equal to  50  "  ;
+
+else
+
+printf   " sorry for inconvinience" ;
+
+
+fi 
+
+
+
+
